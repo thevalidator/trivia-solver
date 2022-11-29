ac363e2905d29f371c951a3e278deb0e96c470ef
thevalidator <the.validator@yandex.ru>
2022-11-30 01:07:50 +0300

    Update Unlim.java

--- src/main/java/ru/thevalidator/galaxytriviasolver/module/trivia/Unlim.java
+++ src/main/java/ru/thevalidator/galaxytriviasolver/module/trivia/Unlim.java
@@ -8,13 +8,16 @@ package ru.thevalidator.galaxytriviasolver.module.trivia;
  * @author thevalidator <the.validator@yandex.ru>
  */
 public enum Unlim {
-    MIN(0.49),
-    MID(1.19),
-    MAX(1.49);
     
+    MIN(1, 0.49),
+    MID(2, 1.19),
+    MAX(3, 1.49);
+    
+    private final int id;
     private final double price;
 
-    private Unlim(double price) {
+    private Unlim(int id, double price) {
+        this.id = id;
         this.price = price;
     }
 
