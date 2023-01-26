c603063aad12b650a04bcfac3b9211d69f508e3c
thevalidator <the.validator@yandex.ru>
2023-01-26 18:02:20 +0300

    Create CanNotCreateWebdriverException.java

---
+++ src/main/java/ru/thevalidator/galaxytriviasolver/exception/CanNotCreateWebdriverException.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.galaxytriviasolver.exception;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class CanNotCreateWebdriverException extends Exception {
+
+    public CanNotCreateWebdriverException(String message) {
+        super(message);
+    }
+
+}
