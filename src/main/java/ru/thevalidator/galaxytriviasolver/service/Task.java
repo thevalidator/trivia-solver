95fbc4851d8ea5d352e1d3aacbff103989e64bee
thevalidator <the.validator@yandex.ru>
2023-01-31 12:53:50 +0300

    Update Task.java

--- src/main/java/ru/thevalidator/galaxytriviasolver/service/Task.java
+++ src/main/java/ru/thevalidator/galaxytriviasolver/service/Task.java
@@ -7,6 +7,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import ru.thevalidator.galaxytriviasolver.exception.LoginErrorException;
+import ru.thevalidator.galaxytriviasolver.exception.TokenNotFoundErrorException;
 import ru.thevalidator.galaxytriviasolver.gui.TriviaMainWindow;
 import ru.thevalidator.galaxytriviasolver.module.base.GalaxyBaseRobot;
 import ru.thevalidator.galaxytriviasolver.module.base.impl.GalaxyBaseRobotImpl;
@@ -19,56 +20,62 @@ public class Task implements Runnable {
 
     private static final Logger logger = LogManager.getLogger(Task.class);
     private static final int TIME_TO_SLEEP_IN_SECONDS = 60;
-    private boolean isActive;
+    public static volatile boolean isActive = false;
     private final State state;
     private final TriviaMainWindow window;
+    private GalaxyBaseRobot robot;
 
     public Task(State state, TriviaMainWindow window) {
         this.state = state;
         this.window = window;
-        this.isActive = false;
+        this.robot = new GalaxyBaseRobotImpl(state);
+        isActive = false;
     }
 
     public boolean isActive() {
         return isActive;
     }
 
-    public void setIsActive(boolean isActive) {
-        this.isActive = isActive;
+    public void setIsActive(boolean value) {
+        synchronized (this) {
+            isActive = value;
+        }
+    }
+    
+    public void hardStopAction() {
+        isActive = false;
+        robot.terminate();
+        ((GalaxyBaseRobotImpl) robot).unregisterObserver(window);
+        window.setStartButtonStatus(-1);
     }
 
     @Override
     public void run() {
         window.setStartButtonStatus(1);
         window.appendToPane("STARTED");
-        GalaxyBaseRobot robot = new GalaxyBaseRobotImpl(state);
+        //GalaxyBaseRobot robot = new GalaxyBaseRobotImpl(state);
         ((GalaxyBaseRobotImpl) robot).registerObserver(window);
         int sleepTimeInSeconds = TIME_TO_SLEEP_IN_SECONDS;
+        
         while (isActive) {
             try {
-
                 robot.login();
-
                 robot.openMail();
-                
                 robot.openGames();
                 robot.selectTriviaGame();
                 if (robot.startTriviaGame()) {
                     robot.playTriviaGame();
                 }
-                
                 sleepTimeInSeconds = robot.getSleepTime();
-
-//                while (isActive) {
-//                    TimeUnit.SECONDS.sleep(10);
-//                }
             } catch (Exception e) {
+                //System.out.println("ERR = " + e.getMessage());
                 logger.error(e.getMessage());
                 String filename = ((GalaxyBaseRobotImpl) robot).getFileNameTimeStamp();
                 ((GalaxyBaseRobotImpl) robot).takeScreenshot(filename + ".png");
                 ((GalaxyBaseRobotImpl) robot).saveDataToFile(filename, e);
                 ((GalaxyBaseRobotImpl) robot).savePageSourceToFile(filename);
-                if (e instanceof LoginErrorException) {
+                if ((e instanceof LoginErrorException) || (e instanceof TokenNotFoundErrorException)) {
+                    window.appendToPane("ERROR: " + e.getMessage());
                     isActive = false;
                 }
             } finally {
@@ -81,12 +88,12 @@ public class Task implements Runnable {
                         window.appendToPane("SLEEPING " + message);
                         TimeUnit.SECONDS.sleep(time);
                     } catch (InterruptedException ex) {
-                        return;
+                        //return;
                     }
                 } else {
                     ((GalaxyBaseRobotImpl) robot).unregisterObserver(window);
                     window.setStartButtonStatus(-1);
-                    window.appendToPane("STOPPED");
+                    window.appendToPane("STOPPED - " + Thread.currentThread().getName());
                 }
             }
         }
