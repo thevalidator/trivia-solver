f1768723d87b6151233d59b3031bcff34328693c
thevalidator <the.validator@yandex.ru>
2023-04-24 16:31:28 +0300

    Create RidesRobot.java

---
+++ src/main/java/ru/thevalidator/galaxytriviasolver/module/trivia/rides/RidesRobot.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.galaxytriviasolver.module.trivia.rides;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public interface RidesRobot {
+    
+    void selectRidesGame();
+    boolean startRidesGame();
+    void playRidesGame();
+
+}
