040cb350843d0f670bcb2ca95a49b48f4314c301
thevalidator <the.validator@yandex.ru>
2023-04-24 16:31:47 +0300

    Create GalaxyAdvancedRobot.java

---
+++ src/main/java/ru/thevalidator/galaxytriviasolver/module/robot/GalaxyAdvancedRobot.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+
+package ru.thevalidator.galaxytriviasolver.module.robot;
+
+import ru.thevalidator.galaxytriviasolver.module.trivia.rides.RidesRobot;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public interface GalaxyAdvancedRobot extends GalaxyBaseRobot, RidesRobot {
+
+}
