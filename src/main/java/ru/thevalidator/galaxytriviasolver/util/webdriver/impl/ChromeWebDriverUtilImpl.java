19b9ffaef1f2c6b47a5870e32762320d95bd21c0
thevalidator <the.validator@yandex.ru>
2023-04-26 23:59:31 +0300

    Update ChromeWebDriverUtilImpl.java

--- src/main/java/ru/thevalidator/galaxytriviasolver/util/webdriver/impl/ChromeWebDriverUtilImpl.java
+++ src/main/java/ru/thevalidator/galaxytriviasolver/util/webdriver/impl/ChromeWebDriverUtilImpl.java
@@ -14,7 +14,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import ru.thevalidator.galaxytriviasolver.exception.CanNotCreateWebdriverException;
 import ru.thevalidator.galaxytriviasolver.exception.ExceptionUtil;
-import ru.thevalidator.galaxytriviasolver.module.trivia.State;
+import ru.thevalidator.galaxytriviasolver.options.ChromeDriverArgument;
+import ru.thevalidator.galaxytriviasolver.options.webdriver.WebDriverArgument;
 import ru.thevalidator.galaxytriviasolver.util.webdriver.WebDriverUtil;
 
 
@@ -23,16 +24,20 @@ public class ChromeWebDriverUtilImpl implements WebDriverUtil {
     private static final Logger logger = LogManager.getLogger(ChromeWebDriverUtilImpl.class);
 
     @Override
-    public WebDriver createWebDriver(State state) throws CanNotCreateWebdriverException {
+    public WebDriver createWebDriver(WebDriverArgument args) throws CanNotCreateWebdriverException {
+        ChromeDriverArgument chromeDriverArgs = (ChromeDriverArgument) args;
         WebDriver webDriver = null;
         try {
             ChromeOptions options = new ChromeOptions();
-            //options.addArguments("--remote-allow-origins=*");
-            if (state.isHeadless()) {
+            if (chromeDriverArgs.isHeadlessMode()) {
                 options.addArguments("--headless=new");
             }
+            if (chromeDriverArgs.hasRemoteAllowOriginsOption()) {
+                options.addArguments("--remote-allow-origins=*");
+            }
+            
             webDriver = new ChromeDriver(options);
-            if (!state.isHeadless()) {
+            if (!chromeDriverArgs.isHeadlessMode()) {
                 webDriver.manage().window().maximize();
             } else {
                 webDriver.manage().window().setSize(new Dimension(1600, 845));
