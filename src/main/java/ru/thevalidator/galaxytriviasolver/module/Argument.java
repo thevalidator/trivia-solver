7d1cb01c558dd8f8fbc02c205430e01a415f0bd1
thevalidator <the.validator@yandex.ru>
2023-04-26 21:49:10 +0300

    Option class -> Argument class

---
+++ src/main/java/ru/thevalidator/galaxytriviasolver/module/Argument.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (C) 2023 thevalidator
+ */
+package ru.thevalidator.galaxytriviasolver.module;
+
+import com.beust.jcommander.Parameter;
+
+/**
+ * @author thevalidator <the.validator@yandex.ru>
+ */
+public class Argument {
+
+    @Parameter(names = { "-d", "--debug" }, description = "Debug mode ON")
+    private boolean hasDebugOption;
+
+    @Parameter(names = { "-h", "--headless" }, description = "Headless mode settings available")
+    private boolean hasHeadlessModeOption;
+    
+    @Parameter(names = { "-r", "--rides" }, description = "Rides play settings available")
+    private boolean hasPlayRidesOption;
+    
+    @Parameter(names = { "-l", "--logoff" }, description = "Loggin off on exit")
+    private boolean hasLogOffOption;
+    
+    @Parameter(names = { "-a", "--advanced" }, description = "Advanced settings available")
+    private boolean hasAdvancedSettingsOption;
+    
+    @Parameter(names = { "-m", "--mail" }, description = "Check mail ON")
+    private boolean hasCheckMailOption;
+    
+    @Parameter(names = { "-w", "--webdriver" }, description = "Custom chrome webdriver path")
+    private String webdriverCustomPath;
+
+    public boolean hasDebugOption() {
+        return hasDebugOption;
+    }
+
+    public boolean hasHeadlessModeOption() {
+        return hasHeadlessModeOption;
+    }
+
+    public boolean hasPlayRidesOption() {
+        return hasPlayRidesOption;
+    }
+
+    public boolean hasLogOffOption() {
+        return hasLogOffOption;
+    }
+
+    public boolean hasAdvancedSettingsOption() {
+        return hasAdvancedSettingsOption;
+    }
+
+    public boolean hasCheckMailOption() {
+        return hasCheckMailOption;
+    }
+
+    public String getWebdriverCustomPath() {
+        return webdriverCustomPath;
+    }
+    
+    
+}
