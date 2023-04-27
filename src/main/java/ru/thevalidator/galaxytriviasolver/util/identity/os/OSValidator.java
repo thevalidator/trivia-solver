e79a1b97f2480daccd3b96fa26f9fbdd08709154
thevalidator <the.validator@yandex.ru>
2023-04-27 17:45:50 +0300

    Update OSValidator.java

--- src/main/java/ru/thevalidator/galaxytriviasolver/util/identity/os/OSValidator.java
+++ src/main/java/ru/thevalidator/galaxytriviasolver/util/identity/os/OSValidator.java
@@ -8,11 +8,12 @@ package ru.thevalidator.galaxytriviasolver.util.identity.os;
  */
 public class OSValidator {
 
-    public static final String OS_NAME = System.getProperty("os.name").toLowerCase();
+    public static final String OS_NAME = System.getProperty("os.name");
+    private static final String OS_LOWER_CASE = OS_NAME.toLowerCase();
     
-    public static final boolean IS_WINDOWS = (OS_NAME.contains("win"));
-    public static final boolean IS_MAC = (OS_NAME.contains("mac"));
-    public static final boolean IS_UNIX = (OS_NAME.contains("nix") || OS_NAME.contains("nux") || OS_NAME.contains("aix"));
-    public static final boolean IS_SOLARIS = (OS_NAME.contains("sunos"));
+    public static final boolean IS_WINDOWS = (OS_LOWER_CASE.contains("win"));
+    public static final boolean IS_MAC = (OS_LOWER_CASE.contains("mac"));
+    public static final boolean IS_UNIX = (OS_LOWER_CASE.contains("nix") || OS_NAME.contains("nux") || OS_NAME.contains("aix"));
+    public static final boolean IS_SOLARIS = (OS_LOWER_CASE.contains("sunos"));
 
 }
