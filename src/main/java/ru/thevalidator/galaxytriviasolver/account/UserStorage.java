bd68f5d0c397c9bbf7d5ab5caa50a82fb8d1e585
thevalidator <the.validator@yandex.ru>
2023-04-28 22:41:22 +0300

    new path for the users file

--- src/main/java/ru/thevalidator/galaxytriviasolver/account/UserStorage.java
+++ src/main/java/ru/thevalidator/galaxytriviasolver/account/UserStorage.java
@@ -7,12 +7,15 @@ import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import ru.thevalidator.galaxytriviasolver.exception.ExceptionUtil;
 import ru.thevalidator.galaxytriviasolver.util.json.JsonUtil;
 
 /**
@@ -21,12 +24,26 @@ import ru.thevalidator.galaxytriviasolver.util.json.JsonUtil;
 public class UserStorage {
 
     private static final Logger logger = LogManager.getLogger(UserStorage.class);
-    public static final String STORAGE_DATE_FILE_NAME = "users.json";
+    public static final String STORAGE_DATE_FILE_NAME = "data" + File.separator + "users.json";
     public static final int MAX_ACCOUNTS = 20;
     private User[] users;
 
     public UserStorage() {
-        users = readUserData().toArray(User[]::new);
+        Path path = Paths.get(STORAGE_DATE_FILE_NAME);
+        if (Files.exists(path)) {
+            users = readUserData().toArray(User[]::new);
+        } else {
+            users = new User[0];
+            try {
+                //Files.createFile(file);
+                File f = new File(STORAGE_DATE_FILE_NAME);
+                f.getParentFile().mkdirs();
+                f.createNewFile();
+            } catch (IOException ex) {
+                //ex.printStackTrace();
+                logger.error(ExceptionUtil.getFormattedDescription(ex));
+            }
+        }
     }
 
     public UserStorage(List<User> users) {
@@ -58,7 +75,7 @@ public class UserStorage {
             File file = Paths.get(UserStorage.STORAGE_DATE_FILE_NAME).toFile();
             users = Arrays.asList(JsonUtil.getMapper().readValue(file, User[].class));
         } catch (IOException e) {
-            logger.error(e.getMessage());
+            logger.error(ExceptionUtil.getFormattedDescription(e));
         }
         return users;
     }
@@ -71,7 +88,7 @@ public class UserStorage {
             ObjectWriter writer = JsonUtil.getMapper().writer(new DefaultPrettyPrinter());
             writer.writeValue(Paths.get(UserStorage.STORAGE_DATE_FILE_NAME).toFile(), users);
         } catch (IOException e) {
-            logger.error(e.getMessage());
+            logger.error(ExceptionUtil.getFormattedDescription(e));
         }
     }
 
