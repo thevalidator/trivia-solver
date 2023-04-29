b11c1070071d5fa1d1d69016d6686797d70f1874
thevalidator <the.validator@yandex.ru>
2023-04-30 02:26:41 +0300

    Update GalaxyAdvancedRobotImpl.java

--- src/main/java/ru/thevalidator/galaxytriviasolver/module/robot/impl/GalaxyAdvancedRobotImpl.java
+++ src/main/java/ru/thevalidator/galaxytriviasolver/module/robot/impl/GalaxyAdvancedRobotImpl.java
@@ -6,12 +6,17 @@ package ru.thevalidator.galaxytriviasolver.module.robot.impl;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import ru.thevalidator.galaxytriviasolver.module.robot.GalaxyAdvancedRobot;
 import ru.thevalidator.galaxytriviasolver.module.robot.Robot;
 import ru.thevalidator.galaxytriviasolver.module.trivia.State;
 import ru.thevalidator.galaxytriviasolver.module.trivia.solver.Solver;
 import ru.thevalidator.galaxytriviasolver.service.Task;
+import ru.thevalidator.galaxytriviasolver.util.webdriver.WebDriverUtil;
+import ru.thevalidator.galaxytriviasolver.web.Locator;
 
 
 public class GalaxyAdvancedRobotImpl extends Robot implements GalaxyAdvancedRobot {
@@ -24,17 +29,71 @@ public class GalaxyAdvancedRobotImpl extends Robot implements GalaxyAdvancedRobo
 
     @Override
     public void selectRidesGame() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Not supported yet");
+//        closePopup(2_500);
+//        WebDriverUtil.wait(driver, 15_000).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath(Locator.getBaseContentIframe())));
+//        ((WebElement)WebDriverUtil.wait(driver, 15_000).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Locator.getGamesRidesBtn(state.getLocale()))))).click();
+//        this.informObservers("opening Rides");
     }
 
     @Override
     public boolean startRidesGame() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Not supported yet");
+//        closePopup(1_500);
+//        WebDriverUtil.wait(driver, 15_000).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath(Locator.getBaseContentIframe())));
+//        final String attempts = ((WebElement)WebDriverUtil.wait(driver, 10_000).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Locator.getRidesGameAttemptsCounter())))).getText().trim();
+//        informObservers(attempts);
+//        if (attempts.equals("0")) {
+//            return false;
+//        }
+//        driver.findElement(By.xpath(Locator.getRidesStartRaceBtn())).click();
+//        return true;
     }
 
     @Override
     public void playRidesGame() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        throw new UnsupportedOperationException("Not supported yet");
+//        int wins = 0;
+//        while (true) {
+//            WebDriverUtil.wait(driver, 15000).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath(Locator.getBaseContentIframe())));
+//            this.informObservers("Race: searching for the opponent");
+//            WebDriverUtil.wait(driver, 25000).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Locator.getRidesWaitOverlay())));
+//            WebDriverUtil.wait(driver, 25000).until(ExpectedConditions.invisibilityOfElementLocated(By.xpath(Locator.getRidesWaitOverlay())));
+//            this.informObservers("Race started");
+//            try {
+//                final long start = System.currentTimeMillis();
+//                while (System.currentTimeMillis() - start < this.state.getNosDelayTime()) {
+//                    Thread.onSpinWait();
+//                }
+//                try {
+//                    ((WebElement)WebDriverUtil.wait(driver, 500).until(ExpectedConditions.elementToBeClickable(By.xpath(Locator.getRidesNitroBtn())))).click();
+//                }
+//                catch (Exception ex) {}
+//                ((WebElement)WebDriverUtil.wait(driver, 15000).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Locator.getRidesPopupCloseBtn())))).click();
+//                while (true) {
+//                    ((WebElement)WebDriverUtil.wait(driver, 3000).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Locator.getRidesPopupCloseBtn())))).click();
+//                }
+//            }
+//            catch (Exception ex2) {
+//                final WebElement resultDiv = (WebElement)WebDriverUtil.wait(driver, 3000).until(ExpectedConditions.visibilityOfElementLocated(By.xpath(Locator.getRidesResultsDiv())));
+//                if (resultDiv.getAttribute("id").contains("lose")) {
+//                    this.informObservers("Race finished - LOST");
+//                }
+//                else {
+//                    this.informObservers("Race finished - WIN");
+//                    ++wins;
+//                }
+//                this.driver.switchTo().defaultContent();
+//                this.closePopup(1500);
+//                WebDriverUtil.wait(driver, 5000).until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.xpath(Locator.getBaseContentIframe())));
+//                final WebElement raceAgainBtn = this.driver.findElement(By.xpath(Locator.getRidesRaceAgainBtn()));
+//                if (raceAgainBtn.getAttribute("action") == null) {
+//                    informObservers("wins: " + wins);
+//                    return;
+//                }
+//                raceAgainBtn.click();
+//            }
+//        }
     }
 
 }
