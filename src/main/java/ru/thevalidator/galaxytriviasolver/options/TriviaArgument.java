b89ce1c0c6ab2c0ab5fc9fb0e251a81293d4b16e
thevalidator <the.validator@yandex.ru>
2023-05-05 19:46:53 +0300

    Update TriviaArgument.java

--- src/main/java/ru/thevalidator/galaxytriviasolver/options/TriviaArgument.java
+++ src/main/java/ru/thevalidator/galaxytriviasolver/options/TriviaArgument.java
@@ -13,50 +13,11 @@ public class TriviaArgument {
     @Parameter(names = { "-d", "--debug" }, description = "Debug mode ON")
     private boolean hasDebugOption;
     
-    @Parameter(names = { "-r", "--rides" }, description = "Rides play settings available")
-    private boolean hasPlayRidesOption;
-    
-    @Parameter(names = { "-l", "--logoff" }, description = "Loggin off on exit")
-    private boolean hasLogOffOption;
-    
-    @Parameter(names = { "-a", "--advanced" }, description = "Advanced settings available")
-    private boolean hasAdvancedSettingsOption;
-    
-    @Parameter(names = { "-m", "--mail" }, description = "Check mail ON")
-    private boolean hasCheckMailOption;
-    
-    @Parameter(names = { "-i", "--imitation" }, description = "Human imitation ON")
-    private boolean hasHumanImitation;
-    
     @Parameter(names = { "-u", "--utility" }, description = "Show in tray only")
     private boolean isUtilityMode;
 
     public boolean hasDebugOption() {
         return hasDebugOption;
-    }
-
-    public boolean hasPlayRidesOption() {
-        return hasPlayRidesOption;
-    }
-
-    public boolean hasLogOffOption() {
-        return hasLogOffOption;
-    }
-
-    public boolean hasAdvancedSettingsOption() {
-        return hasAdvancedSettingsOption;
-    }
-
-    public boolean hasCheckMailOption() {
-        return hasCheckMailOption;
-    }
-
-    public boolean hasHumanImitation() {
-        return hasHumanImitation;
-    }
-
-    public void setHasHumanImitation(boolean hasHumanImitation) {
-        this.hasHumanImitation = hasHumanImitation;
     }
 
     public boolean isUtilityMode() {
